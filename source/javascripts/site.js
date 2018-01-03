7c742afcd1e6b281b1e63304027e4e046590c4cd
alessandrotp <alessandro@thesocialus.com>
2018-01-03 03:03:06 -0400

    Agregado menu de hamburguesa

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,9 @@
 // This is where it all goes :)
+
+// Burger menu
+function myFunction(x) {
+    x.classList.toggle("change");
+    document.getElementsByClassName("burger-items")[0].classList.toggle("change");
+    document.getElementsByClassName("burger-caption")[0].classList.toggle("change");
+	document.getElementsByClassName("burger-caption")[1].classList.toggle("change");
+}
